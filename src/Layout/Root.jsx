49b907f3b2207de197314c5067aa216375c888d2
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-06-04 01:44:35 +0600

    loading spinner added

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigation } from 'react-router';
 import Navbar from '../Components/Header/Navbar';
 import Footer from '../Components/Footer/Footer';
+import Loading from '../Pages/Loading/Loading';
 
 const Root = () => {
+    const navigation = useNavigation();
     return (
         <>
          <header>
             <Navbar></Navbar>
          </header>
          <main>
-            <Outlet></Outlet>
+            <div className='min-h-[calc(100vh-492.31px)]'>
+                {
+                    navigation.state == "loading"? <Loading></Loading> : 
+                    <Outlet></Outlet>
+                }
+            </div>
          </main>
          <footer>
             <Footer></Footer>
