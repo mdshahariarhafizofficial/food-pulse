57d04543142b3beb1f0a181d9227d80760a18b20
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-06-09 15:23:10 +0600

    Nearly Expiring Items section added

---
+++ src/Components/NearlyExpiryItems/NearlyExpiryItems.jsx
@@ -0,0 +1,85 @@
+import { AlarmClock } from 'lucide-react';
+import FoodCard from '../FoodCard/FoodCard';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
+const NearlyExpiryItems = ({expiringSoon}) => {
+    const expiringFood = expiringSoon;
+
+    // Slider
+      var settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    autoplay: true,
+    speed: 500,
+    autoplaySpeed: 2000,
+    
+    cssEase: "linear",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
+
+    return (
+        <div className='py-10'>
+            <div className='text-center mb-6'>
+                <h2 className='flex items-center justify-center gap-2 text-3xl lg:text-5xl font-bold'><AlarmClock size={80} color='#eb0029' /> Nearly Expiring Items</h2>
+                <p className='text-secondary font-medium'>Don't let them go to waste!</p>
+            </div>
+
+            {/* Slider */}
+                <div className="slider-container">
+                <Slider {...settings}>
+                    {
+                    expiringFood.map(food => ( 
+                    <div key={food._id} className='px-4'>
+                        <FoodCard 
+                            food={food}
+                        ></FoodCard>
+                    </div>
+                    ))
+                }
+                </Slider>
+                </div>
+
+            {/* <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+                {
+                    expiringFood.map(food => <FoodCard
+                        key={food._id} 
+                        food={food}
+                    ></FoodCard>)
+                }
+            </div> */}
+
+        </div>
+    );
+};
+
+export default NearlyExpiryItems;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Slider from '../../Components/Slider/Slider';
+import NearlyExpiryItems from '../../Components/NearlyExpiryItems/NearlyExpiryItems';
 
 const Home = () => {
+    const [expiringSoon, setExpiringSoon] = useState([]);
+
+    // Load Expire Soon Foods
+    useEffect(() => {
+        fetch('http://localhost:8000/foods/expiring-soon').then(res => res.json())
+        .then(data => {
+            setExpiringSoon(data)
+        })
+    }, [])
+
     return (
-        <div className=''>
-            <div className=''>
+        <>
+            <div>
                 <Slider></Slider>
+                <div className='bg-[#f4f1ea] pb-20 pt-4 px-2'>
+                    <div className='max-w-[1500px] mx-auto'>
+                        <NearlyExpiryItems expiringSoon = {expiringSoon}></NearlyExpiryItems>
+                    </div>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
