4dadfa0fa0c536691b2ff0c7624182b4e99c1396
mdshahariarhafizofficial <shahariarwebdev@gmail.com>
2025-06-11 15:06:54 +0600

    add framer animation in slider

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -18,7 +18,7 @@ import slide2 from '../../assets/slide2.jpg';
 import slide3 from '../../assets/slide3.jpg';
 import slide4 from '../../assets/slide4.jpg';
 import { Link } from 'lucide-react';
-
+import { motion } from 'framer-motion';
 
 const Slider = () => {
     return (
@@ -27,7 +27,7 @@ const Slider = () => {
         spaceBetween={30}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 3000,
           disableOnInteraction: false,
         }}
         pagination={{
@@ -47,12 +47,28 @@ const Slider = () => {
           <div className="absolute inset-0 bg-[#000000] opacity-30"></div>
 
         {/* Content */}
-        <div className="relative z-10 text-white p-10 space-y-7">
-            <h2 className="text-4xl md:text-8xl text-shadow-lg/80 text-shadow-black font-bold">Build Connections, One Meal at a Time</h2>
-            <p className='text-shadow-lg/80 text-shadow-black'>Discover food lovers near you. Share, manage, and <br /> track your fridge like never before.</p>
+        <motion.div 
+              initial={{ opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8 }}        
+        className="relative z-10 text-white p-10 space-y-7">
+            <motion.h2 
+            initial={{ y: -50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 0.8 }}
+            className="text-4xl md:text-8xl text-shadow-lg/80 text-shadow-black font-bold">Build Connections, One Meal at a Time</motion.h2>
+            <motion.p
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1, delay: 0.4 }}            
+            className='text-shadow-lg/80 text-shadow-black'>Discover food lovers near you. Share, manage, and <br /> track your fridge like never before.</motion.p>
 
             {/* Type Writer */}
-            <div className='space-y-4'>
+            <motion.div 
+              initial={{ scale: 0.8, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 0.8, delay: 0.8 }}             
+            className='space-y-4'>
               <h2 className='text-4xl md:text-4xl text-shadow-lg/80 text-shadow-black font-bold'>Find your food in → 
               </h2>
                 <h4 className='text-xl md:text-2xl text-shadow-lg/80 text-shadow-black font-bold text-primary'>
@@ -66,13 +82,17 @@ const Slider = () => {
                     delaySpeed={1000}
                  />
                 </h4>              
-            </div>
-            <div>
+            </motion.div>
+            <motion.div
+              initial={{ y: 30, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 0.8, delay: 1.2 }}             
+            >
               <a href='/fridge'>
                 <button className='btn btn-primary px-10 py-6 text-xl'>Discover Foods <BsArrowRightSquareFill /></button>
               </a>
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
 
         </div>
         </SwiperSlide>
